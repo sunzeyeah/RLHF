a6ae2e052daa0ea116d8f5ad3d36be0002f17ca4
sunzeyeah <sunzeyeah@sina.com>
2023-03-14 18:21:45 +0800

    update README.md and rearrange repo

--- README.md
+++ README.md
@@ -1,53 +1,88 @@
-forked from [Pangu-RLHF](https://openi.pcl.ac.cn/PCL-Platform.Intelligence/Pangu-RLHF)
+## Features
 
-## 基于`trlx`库使用RLHF训练Pangu 2.6B中文对话模型pipeline
+---
+提供2大功能：
+- LLM模型评测：参考GPT类模型，基于ZeroShot和FewShot实现 
+- ChatGPT模型训练pipeline：根据[Learning to Summarize from human feedback](https://arxiv.org/abs/2009.01325) ，实现3大流程: SFT、Reward Model和RLHF
 
-我们的pipeline是基于OpenAI论文 "[Learning to Summarize from human feedback](https://arxiv.org/abs/2009.01325)"的开源代码进行修改。
+## Setup
 
-### 准备阶段
-
-1).  需要配置trlx库相关环境，参考 "[trlx] (https://github.com/CarperAI/trlx)"
-
+---
+1. Install apex
 ```bash
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install --global-option="--cpp_ext" --global-option="--cuda_ext" --no-cache -v --disable-pip-version-check .  2>&1 | tee build.log
+```
+
+2. Install deepspeed
+```bash
+git clone https://github.com/microsoft/deepspeed
+cd deepspeed
+rm -rf build
+TORCH_CUDA_ARCH_LIST="7.0" DS_BUILD_CPU_ADAM=1 DS_BUILD_AIO=1 DS_BUILD_UTILS=1 pip install -e . --global-option="build_ext" --global-option="-j8" --no-cache -v --disable-pip-version-check
+```
+根据下图，调整```TORCH_CUDA_ARCH_LIST="7.0"```为对应的NVIDIA GPU架构
+![image info](./images/torch_cuda_list.png "torch_cuda_list")
+
+3. install trlx
+```bash
+pip install -r requirements.txt
 git clone https://github.com/CarperAI/trlx.git
 cd trlx
-pip install torch --extra-index-url https://download.pytorch.org/whl/cu116 # for cuda
 pip install -e .
 ```
 
-2).  下载盘古-2.6B模型: 
+## Data & Model Download
+
+---
+
+1. 模型下载
+
+| 模型      | size | 百度网盘地址  |  提取码      | 
+| ----------- | ----------- | ----------- |  ----------- |
+| [Pangu-350M](https://openi.pcl.ac.cn/PCL-Platform.Intelligence/PanGu-Alpha)   | 659MB | [Pangu-350M](https://pan.baidu.com/s/1IzgtW48S2PKyjxPPMe1rAQ) |  c5jj |
+| [Pangu-2.6B](https://openi.pcl.ac.cn/PCL-Platform.Intelligence/PanGu-Alpha)   | 9.8GB | [Pangu-2.6B](https://pan.baidu.com/s/1Tzvja4q_LgQOwkWPQ4jShw)    | 2rad |
+| [Pangu-13B](https://openi.pcl.ac.cn/PCL-Platform.Intelligence/PanGu-Alpha)   | 23.6GB | [Pangu-13B](https://pan.baidu.com/s/11fWAeYYKqI7pH0UiuJ5jEQ)    | u3dx |
+| [GLM-335M-chinese](https://github.com/THUDM/GLM) | 679MB | [GLM-335M-chinese](https://pan.baidu.com/s/11Lef-E7Tsz5OGOueCpiqaA) | ii8e |
+| [GLM-10B-chinese](https://github.com/THUDM/GLM)   |  |      |   |
+
+2. 数据下载
+
+| 数据集      | size | 百度网盘地址  |  提取码      | 
+| ----------- | ----------- | ----------- |  ----------- |
+| CLUE Benchmark   | 500MB | [CLUE Benchmark](https://pan.baidu.com/s/15F60nRbBd6d6UvyXdwbXQA) |  m6gt |
+| SFT & Reward Data  | 4GB | [SFT & Reward Data](https://pan.baidu.com/s/1QRxtNZYTd2N_zOwqzfzvRw) |  ueiy |
+
+SFT & Reward Data基于百科、知道问答、对联、古文、古诗词、微博数据构造，用于SFT和Reward阶段训练
+
+
+## Usage
+
+---
+#### LLM模型评测
 
 ```bash
-    https://huggingface.co/imone/pangu_2_6B
+cd examples
+bash eval_pretrain.sh
 ```
 
-3).  准备SFT数据集(以webtext为例): 
+#### SFT
 
 ```bash
-    https://paperswithcode.com/dataset/webtext
+cd examples
+bash eval_pretrain.sh
 ```
-保存至: ./dialogue_dir/demo.json
+#### Reward Molde
 
-4).  收集人工反馈数据
-保存至: ./reward_data_dir/processed/demo.json
+```bash
+cd examples
+bash train_reward.sh
+```
 
+#### RLHF
 
-### 训练步骤
-
-1). 监督微调 (SFT):
-
-    cd sft/ && deepspeed train_SFT.py
-
-2). 训练 Reward 模型:
-
-    cd reward_model/ && deepspeed train_reward_model.py
-
-3). 使用PPO算法强化学习:
-
-    accelerate launch --config_file configs/default_accelerate_config.yaml trlx_pangu_rlhf.py
-
-   备注: 至少需要1张V100显卡。
-
-## 参考文献
-
-1. Nisan Stiennon, Long Ouyang, Jeff Wu, Daniel M. Ziegler, Ryan Lowe, Chelsea Voss, Alec Radford, Dario Amodei, Paul Christiano, "[Learning to Summarize from human feedback](https://arxiv.org/abs/2009.01325)", Neural Information Processing Systems, 2020.
+```bash
+cd examples
+bash train_rlhf.sh
+```
---
+++ src/data_prepare.py
@@ -0,0 +1,398 @@
+
+import os
+import time
+import json
+import argparse
+import random
+import glob
+import pandas as pd
+import opencc
+
+from tqdm import tqdm
+from transformers import AutoTokenizer
+
+from src.utils.logger import logger
+from src.utils.file_utils import set_seed
+from src.utils.nlp_utils import clean_text
+
+
+def weibo_summary_comment(args, tokenizer):
+    t = time.time()
+    fi = os.path.join(args.data_dir, "weibo_summary_comments_json.json")
+    fo = os.path.join(args.output_dir, "weibo_summary_comments.jsonl")
+    data = []
+    with open(fo, "w", encoding="utf-8") as w:
+        with open(fi, "r", encoding="utf-8") as r:
+            while True:
+                line = r.readline()
+                if not line:
+                    break
+
+                item = json.loads(line.strip("\n"))
+                article = item['article'].replace(" ", "")
+                abstract = item['abstract'].replace(" ", "")
+                prompt = f"新闻内容：{article}{tokenizer.sep_token}摘要：{abstract}{tokenizer.sep_token}评论："
+                answers = [
+                    {
+                        "answer": k.replace(" ", ""),
+                        "score": int(v)
+                    } for (k, v) in sorted(item['comments'], key=lambda x: (int(x[1]), len(x[0])), reverse=True)
+                ]
+                w.write(json.dumps({"prompt": prompt, "answers": answers}, ensure_ascii=False)+'\n')
+                data.append({"prompt": prompt, "answers": answers})
+    logger.info(f"length: {len(data)}, time taken: {time.time()-t} s")
+
+    return data
+
+
+def couplets(args, tokenizer):
+    t1 = time.time()
+    fi = os.path.join(args.data_dir, "couplets.txt")
+    fo = os.path.join(args.output_dir, "couplets.jsonl")
+    l2 = []
+    nexts = dict()
+    with open(fi, "r", encoding="utf-8") as r:
+        while True:
+            line = r.readline()
+            if not line:
+                break
+            line = line.strip("\n")
+            idx = len(line) // 2
+            prompt = line[:idx]
+            answer = line[idx+1:]
+            answers = [{"answer": answer, "score": 1}]
+            l2.append({"prompt": f"上联：{prompt}{tokenizer.sep_token}下联：", "answers": answers})
+            length = len(answer)
+            if length not in nexts:
+                nexts[length] = list()
+            nexts[length].append(answer)
+    t2 = time.time()
+    logger.info(f"length: {len(l2)}, # different lengths: {len(nexts)}, time taken: {t2-t1} s")
+    data = []
+    with open(fo, "w", encoding="utf-8") as w:
+        for l in tqdm(enumerate(l2), desc="Processing Couplets"):
+            answer = l['answers'][0]
+            length = len(answer['answer'])
+            # 上下联长度一样
+            nexts_tmp = set(nexts[length])
+            nexts_tmp.remove(answer['answer'])
+            nexts_tmp = set(nexts[length]).difference(set([answer['answer']]))
+            #         nexts_tmp.remove(answer['answer'])
+            answers.extend([{"answer": fa, "score": 0} for fa in random.sample(nexts_tmp, 2)])
+            # 上下联长度不一样
+            keys = set(nexts.keys())
+            keys.remove(length)
+            answers.extend([{"answer": random.choice(nexts[key]), "score": -1} for key in random.sample(keys, 2)])
+            #         answers = sorted(answers, key=lambda x: x['score'], reverse=True)
+            w.write(json.dumps({"prompt": l['prompt'], "answers": answers}, ensure_ascii=False)+'\n')
+            data.append({"prompt": l['prompt'], "answers": answers})
+    #         if i % 1000 == 0:
+    #             logger.info(f"{i} samples processed, time taken: {time.time()-t2} s")
+    logger.info(f"length: {len(data)}, time taken: {time.time()-t2} s")
+
+    return data
+
+
+def zhidao(args, tokenizer):
+    t = time.time()
+    fp = os.path.join(args.data_dir, "zhidao", "*.csv")
+    fo = os.path.join(args.output_dir, "zhidao.jsonl")
+    data = []
+    with open(fo, "w", encoding="utf-8") as w:
+        for fi in glob.glob(fp):
+            df = pd.read_csv(fi).sort_values(by=["title", "is_best"], ascending=False)
+            prev_title = None
+            prev_prompt = None
+            for _, val in df.iterrows():
+                if isinstance(val['question'], str) and val['question'] != val['title']:
+                    prompt = f"问题：{val['title']}{tokenizer.sep_token}内容：{val['question']}{tokenizer.sep_token}回答："
+                else:
+                    prompt = f"问题：{val['title']}{tokenizer.sep_token}回答："
+                if prev_title is not None and prev_title == val['title']:
+                    answers.append({"answer": val['reply'], "score": val['is_best']})
+                else:
+                    if prev_title is not None:
+                        #                     l3.append({"prompt": prev_prompt, "answers": copy.deepcopy(answers)})
+                        w.write(json.dumps({"prompt": prev_prompt, "answers": answers}, ensure_ascii=False)+'\n')
+                        data.append({"prompt": prev_prompt, "answers": answers})
+                    answers = [{"answer": val['reply'], "score": val['is_best']}]
+                prev_prompt = prompt
+                prev_title = val['title']
+            #         l3.append({"prompt": prev_prompt, "answers": copy.deepcopy(answers)})
+            w.write(json.dumps({"prompt": prev_prompt, "answers": answers}, ensure_ascii=False)+'\n')
+            data.append({"prompt": prev_prompt, "answers": answers})
+            logger.info(f"finished processing {os.path.basename(fi)}")
+    logger.info(f"length: {len(data)}, time taken: {time.time()-t} s")
+
+    return data
+
+
+def chinese_classical(args, tokenizer):
+    t1 = time.time()
+    fp = os.path.join(args.data_dir, "Classical-Modern", "bitext", "*")
+    fo = os.path.join(args.output_dir, "chinese_classical.jsonl")
+    l3 = []
+    dicts = dict()
+    for fi in glob.glob(fp):
+        name = os.path.basename(fi)
+        dicts[name] = {"古文": [], "现代文": []}
+        with open(fi, "r", encoding="utf-8") as r:
+            for i, line in enumerate(r):
+                line = line.strip("\n")
+                if line.startswith("古文"):
+                    p1 = line[3:]
+                    dicts[name]['古文'].append(p1)
+                elif line.startswith("现代文"):
+                    p2 = line[4:]
+                    dicts[name]['现代文'].append(p2)
+                elif p1 is not None and p2 is not None:
+                    pair = [("古文", p1), ("现代文", p2)]
+                    random.shuffle(pair)
+                    prompt = f"{pair[0][0]}：{pair[0][1]}{tokenizer.sep_token}{pair[1][0]}："
+                    answers = [{"answer": pair[1][1], "score": 1}]
+                    l3.append({"prompt": prompt, "answers": answers, "name": name})
+                    p1 = None
+                    p2 = None
+    t2 = time.time()
+    logger.info(f"length: {len(l3)}, # different names: {len(dicts)}, time taken: {t2-t1} s")
+    data = []
+    with open(fo, "w", encoding="utf-8") as w:
+        for l in tqdm(enumerate(l3), desc="Processing Chinese Classical-Modern"):
+            name = l['name']
+            prompt = l['prompt']
+            answer = l['answers'][0]['answer']
+            if prompt.startswith("古文"):
+                answer_type = '现代文'
+            else:
+                answer_type = '古文'
+            samples_tmp = set(dicts[name][answer_type])
+            samples_tmp.remove(answer)
+            answers.extend([{"answer": fa, "score": 0} for fa in random.sample(samples_tmp, 2)])
+            keys = set(dicts.keys())
+            keys.remove(name)
+            answers.extend([{"answer": random.choice(dicts[key][answer_type]), "score": -1} for key in random.sample(keys, 2)])
+            w.write(json.dumps({"prompt": prompt, "answers": answers}, ensure_ascii=False)+'\n')
+            data.append({"prompt": prompt, "answers": answers})
+    #         if i % 100 == 0:
+    #             logger.info(f"{i} samples processed, time taken: {time.time()-t2} s")
+    logger.info(f"length: {len(data)}, time taken: {time.time()-t2} s")
+
+    return data
+
+
+def chinese_poetry(args, tokenizer):
+    converter = opencc.OpenCC('t2s.json')
+    t1 = time.time()
+    fp = [
+        # 四书五经
+        os.path.join(args.data_dir, "chinese-poetry", "lunyu", "lunyu.json"),
+        os.path.join(args.data_dir, "chinese-poetry", "sishuwujing", "*.json"),
+        # 古体诗
+        os.path.join(args.data_dir, "chinese-poetry", "caocaoshiji", "caocao.json"),
+        os.path.join(args.data_dir, "chinese-poetry", "shijing", "shijing.json"),
+        # 楚辞
+        os.path.join(args.data_dir, "chinese-poetry", "chuci", "chuci.json"),
+        # 诗
+        os.path.join(args.data_dir, "chinese-poetry", "shi", "poet*.json"),
+        # 词
+        os.path.join(args.data_dir, "chinese-poetry", "ci", "ci*.json"),
+        os.path.join(args.data_dir, "chinese-poetry", "nalanxingde", "*.json"),
+        os.path.join(args.data_dir, "chinese-poetry", "wudai", "huajianji", "*juan.json"),
+        os.path.join(args.data_dir, "chinese-poetry", "wudai", "nantang", "poetrys.json"),
+        # 曲
+        os.path.join(args.data_dir, "chinese-poetry", "yuanqu", "yuanqu.json"),
+    ]
+    fs = [each for f in fp for each in glob.glob(f)]
+    
+    l5 = []
+    dicts = dict()
+    for fi in fs:
+        lines = json.load(open(fi, "r", encoding="utf-8"))
+        if isinstance(lines, dict):
+            lines = [lines]
+        for i, line in enumerate(lines):
+            if "lunyu" in fi:
+                author = "孔子"
+                genre = "经书"
+                title = line['chapter']
+                contents = "".join(line['paragraphs'])
+            elif "daxue" in fi:
+                author = "曾子"
+                genre = "经书"
+                title = "大学"
+                contents = converter.convert("".join(line['paragraphs'])).replace("「", "“").replace("」", "”")
+            elif "mengzi" in fi:
+                author = "孟子"
+                genre = "经书"
+                title = converter.convert(line['chapter'])
+                contents = converter.convert("".join(line['paragraphs'])).replace("「", "“").replace("」", "”")
+            elif "zhongyong" in fi:
+                author = "孔伋"
+                genre = "经书"
+                title = "中庸"
+                contents = converter.convert("".join(line['paragraphs'])).replace("「", "“").replace("」", "”")
+            elif "caocao" in fi:
+                author = "曹操"
+                genre = "古体诗"
+                title = line['title']
+                contents = "".join(line['paragraphs'])
+            elif "shijing" in fi:
+                author = "诗经"
+                genre = "古体诗"
+                title = line['chapter'] + "-" + line['section'] + "-" + line['title']
+                contents = "".join(line['content'])
+            elif "chuci" in fi:
+                author = line['author']
+                genre = "楚辞"
+                title = line['section'] + "-" + line['title']
+                contents = "".join(line['content'])
+            elif "nalanxingde" in fi:
+                author = line['author']
+                genre = "词"
+                title = line['title']
+                contents = "".join(line['para'])
+            elif "huajianci" in fi:
+                author = line['author']
+                genre = "词"
+                title = line['title']
+                contents = "".join(line['paragraphs'])
+            elif "nantang" in fi:
+                author = line['author']
+                genre = "词"
+                title = line['title']
+                contents = "".join(line['paragraphs'])
+            elif "yuanqu" in fi:
+                author = line['author']
+                genre = "曲"
+                title = line['title']
+                contents = "".join(line['paragraphs'])
+            elif "shi" in fi:
+                if len(line['paragraphs']) <= 0:
+                    continue
+                author = converter.convert(line['author'])
+                genre = "五言诗" if len(line['paragraphs'][0]) == 12 else "七言诗"
+                title = converter.convert(line['title'])
+                contents = converter.convert("".join(line['paragraphs']))
+            elif "ci" in fi:
+                author = line['author']
+                genre = "词"
+                title = line['rhythmic']
+                contents = "".join(line['paragraphs'])
+            if genre not in dicts:
+                dicts[genre] = dict()
+            if author not in dicts[genre]:
+                dicts[genre][author] = dict()
+            quantifier = "篇" if genre in ["经书", "楚辞"] else "首"
+            prompt = f"以{author}的风格，写一{quantifier}{genre}，题为{title}{tokenizer.sep_token}"
+            answers = [{"answer": contents, "score": 1}]
+            l5.append({"prompt": prompt, "answers": answers, "genre": genre, "title": title, "author": author})
+            dicts[genre][author][title] = contents
+    t2 = time.time()
+    logger.info(f"length: {len(l5)}, # different lengths: {len(dicts)}, time taken: {t2-t1} s")
+    data = []
+    fo = os.path.join(args.output_dir, "chinese_poetry.jsonl")
+    with open(fo, "w", encoding="utf-8") as w:
+        for l in tqdm(enumerate(l5), desc="Processing Chinese Poetry"):
+            genre = l['genre']
+            author = l['author']
+            title = l['title']
+            prompt = l['prompt']
+            answers = l['answers']
+            # 同作者其他作品-2
+            titles_tmp = set(dicts[genre][author].keys())
+            titles_tmp.remove(title)
+            if len(titles_tmp) > 0:
+                t = random.choice(list(titles_tmp))
+                answers.append({"answer": dicts[genre][author][t], "score": 0})
+            # 同体裁其他作者其他作品-1
+            authors_tmp = set(dicts[genre].keys())
+            authors_tmp.remove(author)
+            a = random.choice(list(authors_tmp))
+            t = random.choice(list(dicts[genre][a].keys()))
+            answers.append({"answer": dicts[genre][a][t], "score": -1})
+            # 不同体裁作品-0
+            genres_tmp = set(dicts.keys())
+            genres_tmp.remove(genre)
+            g = random.choice(list(genres_tmp))
+            a = random.choice(list(dicts[g].keys()))
+            t = random.choice(list(dicts[g][a].keys()))
+            answers.append({"answer": dicts[g][a][t], "score": -2})
+            w.write(json.dumps({"prompt": prompt, "answers": answers}, ensure_ascii=False)+'\n')
+            data.append({"prompt": prompt, "answers": answers})
+    logger.info(f"length: {len(data)}, time taken: {time.time()-t2} s")
+
+    return data
+    
+
+def baike_qa_2019(args, tokenizer):
+    fs = glob.glob(os.path.join(args.data_dir, "baike_qa2019", "baike_qa_*.json"))
+    fo = os.path.join(args.output_dir, "baike_qa.jsonl")
+    data = []
+    t = time.time()
+    with open(fo, "w", encoding="utf-8") as w:
+        for f in fs:
+            with open(f, "r", encoding="utf-8") as r:
+                while True:
+                    line = r.readline()
+                    if not line:
+                        break
+                    item = json.loads(line.strip("\n"))
+                    question = clean_text(item['title'] if len(item['title']) > len(item['desc']) else item['desc'])
+                    prompt = f"{question}{tokenizer.sep_token}回答："
+                    answer = clean_text(item['answer'])
+                    answers = [{"answer": answer, "score": 1}]
+                    w.write(json.dumps({"prompt": prompt, "answers": answers}, ensure_ascii=False)+'\n')
+                    data.append({"prompt": prompt, "answers": answers})
+    logger.info(f"length: {len(data)}, time taken: {time.time()-t} s")
+
+    return data
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--data_dir", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--model_name_or_path", type=str, required=True)
+
+    parser.add_argument("--seed", type=int, default=42)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_parser()
+    logger.info(f"Parameters: {args}")
+    # set random seed
+    set_seed(args.seed)
+    # load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, use_cache=False, trust_remote_code=True)
+
+    # process raw datasets
+    data_weibo = weibo_summary_comment(args, tokenizer)
+    data_couplets = couplets(args, tokenizer)
+    data_zhidao = zhidao(args, tokenizer)
+    data_chinese_classical = chinese_classical(args, tokenizer)
+    data_chinese_poetry = chinese_poetry(args, tokenizer)
+    data_baike = baike_qa_2019(args, tokenizer)
+
+    # merge processed datasets
+    data = data_weibo + data_couplets + data_zhidao + data_chinese_classical + data_chinese_poetry + data_baike
+    random.shuffle(data)
+    fo = os.path.join(args.output_dir, "train_data_external_v1.jsonl")
+    with open(fo, "w", encoding="utf-8") as w:
+        for d in data:
+            w.write(json.dumps(d, ensure_ascii=False)+'\n')
+
+    fo = os.path.join(args.output_dir, "dev_data_external_v1.jsonl")
+    with open(fo, "w", encoding="utf-8") as w:
+        for d in data[:10000]:
+            w.write(json.dumps(d, ensure_ascii=False)+'\n')
+
+    logger.info("Finished saving processed train & dev files")
+
+
+if __name__ == "__main__":
+    main()
