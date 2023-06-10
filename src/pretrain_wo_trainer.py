aac7344301358008993875754a2fb563fc6dbbb0
sunzeyeah <sunzeyeah@sina.com>
2023-06-10 10:29:44 +0800

    update pretrain w/o trainer

--- src/pretrain_wo_trainer.py
+++ src/pretrain_wo_trainer.py
@@ -152,9 +152,9 @@ def main():
 
     # load tokenizer and model
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, use_cache=False, trust_remote_code=True)
-    if "pangu" in args.model_name_or_path:
-        model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, use_cache=False, trust_remote_code=True)
-        model.resize_token_embeddings(tokenizer.vocab_size)
+    if "llama" in args.model_name_or_path:
+        model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, use_cache=False, trust_remote_code=True).half()
+        # model.resize_token_embeddings(tokenizer.vocab_size)
         # model.config.pad_token_id = tokenizer.pad_token_id
         # model.config.bos_token_id = tokenizer.bos_token_id
         # model.config.eos_token_id = tokenizer.eos_token_id
